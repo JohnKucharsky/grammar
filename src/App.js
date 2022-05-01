81308496d26637839a28bee1f922778c6c5be014
John <vannok@live.com>
2022-05-01 18:27:32 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,178 @@
-import logo from './logo.svg';
-import './App.css';
+const data = [
+  {
+    tags: ["Tenses-времена", "Present", "Present Simple and Present Continous"],
+    title:
+      "Present Simple-настоящее простое and Present Continous-настоящее длительное",
+    content:
+      "My family usually watch films online. We don't often go to the cinema and prefer sitting on the sofa with snacks. But today, we are going to the cinema. We aren't staying at home. I always drink coffee in the morning. Liz is watering the plants right now. We work every day. My neighbors are having a party at the moment. I go to the gym twice a week. Are you still playing computer games? How often are you late for work? She doesn't like sports, but today she is going to the gym. He is ill today. I am not often tired.",
+  },
+  {
+    tags: ["Tenses-времена", "Present", "Present Perfect and Past Simple"],
+    title:
+      "Present Perfect-настоящее совершенное and Past Simple-простое прошедшее",
+    content:
+      "— Where's Andy?— He's not here. He's gone to the hospital.— When did he go out?— He left at 6 o'clock. Maddison has lived in many places. She lived in the USA when she was a child. Five years ago, she lived in Brazil. Last year, she lived in Belgium. Now, she lives in Congo. I have never been abroad. My sister has cooked a lot of food for the party. We moved to France in 2010. Kyle liked corn when he was a child. I haven't read any of Agatha Christie's books. Jake hasn't been home since Christmas. I didn't watch anything last night. Millie didn't work last Friday. Have you ever kissed a stranger? How has your family been lately? Did you eat mushrooms when you were younger? When did you learn to swim?",
+  },
+  {
+    tags: [
+      "Verb-глагол",
+      "Indicative Mood-изъявительное наклонение",
+      "Verbals-неличные",
+      "Gerund-герундий",
+    ],
+    title: "Verb + -ing after like",
+    content:
+      "Most people like having a holiday in summer, and I love going to hot countries. My favourite destination is Dubai. It’s a great place for family holidays. But I hate taking long flights.",
+  },
+  {
+    tags: [
+      "Verb-глагол",
+      "Indicative Mood-изъявительное наклонение",
+      "Passive Voice-Пассивный залог",
+    ],
+    title: "Passive ('be' and Past Participle-причастие прошедшего времени)",
+    content:
+      "Rice is grown in China. A company party was organised yesterday, but I wasn’t invited. I am never invited to parties. In the next 20 years, the cure for cancer will be developed by scientists. Our office is cleaned every day. My room is being redecorated at the moment. My bike has been stolen! Harry Potter books were written by J. K. Rowling. My watch will be repaired tomorrow. Uniforms must be worn at all times. Vitamins should be taken regularly.",
+  },
+  {
+    tags: [
+      "Verb-глагол",
+      "Indicative Mood-изъявительное наклонение",
+      "Imperative Mood-совелительное",
+      "Subjunctive Mood-сослагательное",
+      "Reported Speech-косвенная речь",
+    ],
+    title: "Reported speech. Reporting Questions",
+    content:
+      "Kate said to Peter, 'My dog is sick. Do you know any good vets?' Peter said to Jane, 'Kate told me that her dog was sick. She asked me if I knew any good doctors.' Ally asks John, 'What will we study next week? I have missed some classes, so I don't know' John tells Sarah, 'Ally asked me what we would study the week after. She said that she had missed some classes, so she didn’t know'.I am married. She told me (that) she was married. Jake wants to swim. I said (that) Jake wanted to swim. We are walking in the park. They told me (that) they were walking in the park. We saw Sue yesterday. They said (that) they had seen Sue the day before. I have never been to France. I told him (that) I had never been to France. I will write to you tomorrow. Mike said (that) he would write to me the next day. Can you swim? She asked me if I could swim. Do you like milk? They asked her whether she liked milk. Where is Mary? Tom asked where Mary was. Where were you yesterday? Lea asked me where I had been the day before.",
+  },
+  {
+    tags: ["Verb-глагол", "Conditionals-Условные предложения"],
+    title: "First Conditional",
+    content:
+      "If I take the bus to the cinema, I will be late. If I’m late, Alice will get angry. And if Alice gets angry, the evening will be ruined! I’ll get a taxi. If I have time, I will help you. If Mark eats more healthy food, he will lose some weight.If you don’t hurry, we’re going to miss the plane. If you ask Jane, she can help you. If we call a taxi, we might get there sooner. If the weather is nice, we’ll go to the beach. = We’ll go to the beach if the weather is nice.",
+  },
+  {
+    tags: ["Verb-глагол", "Conditionals-Условные предложения"],
+    title: "Second Conditional",
+    content:
+      "Dan wants a new laptop to play video games, but he doesn’t have much money. If he had more money, he would buy a new laptop. He would have more money if he worked harder. But if he worked harder, he wouldn’t have time to play video games. And if he didn’t play video games, he wouldn’t need a new laptop. If I had a million dollars, I would give it all to charity. If Andy wasn’t so busy, he would spend more time travelling. If you weren’t busy, we could watch a film. If you told Jane about your problem, she might be able to help you. If I had time today, I would go to the museum. = I would go to the museum if I had time today.",
+  },
+  {
+    tags: ["Verb-глагол", "Auxilary Verbs-вспомогательные глаголы", "Used to"],
+    title: "Used to or Didn't Use to",
+    content:
+      "Mark used to be a sales assistant. He used to work in a supermarket. Now he’s a regional manager in a big company. When Annie was a teenager, she didn’t use to have long hair. I used to play tennis when I was a student.Jason used to smoke a lot. Ally didn’t use to like mushrooms when she was a child. I didn’t use to get up so early before. Where did you use to work before you came here? Did you use to walk your dog every day?",
+  },
+  {
+    tags: ["Verb-глагол", "Modal Verbs-модальные глаголы"],
+    title: "Modal Verbs (Have to, Don't have to, Must, Mustn't)",
+    content:
+      "You have to be quiet in the library. You must have a reader's card to borrow books. You don't have to switch off your phone, but you have to put it in silent mode. You mustn't eat in the library. You must get regular medical check-ups. You mustn't draw on pictures in a museum. Must I do this? I have to get up at 6 to come to the office on time. Mike has to pass the test to get his driving licence. We had to clean the house after the party. Do I have to wash the dishes? You don't have to use a pen, you can use a pencil. Dan has an easy job, so he doesn't have to work hard. There was no queue at the hospital, so we didn't have to wait long.",
+  },
+  {
+    tags: ["Verb-глагол", "Multiword Verbs-сложные глаголы"],
+    title: "Word order of Phrasal verbs",
+    content:
+      "I put my sweater on, turned on my favourite music and picked out the best tea. I’m ready for a nice evening. Could you turn down the TV? Actually, turn it off completely. I need some quiet. Your teddy bear is on the floor. Pick him up and put him down on your bed. Why do you never clean up your room? I need to give Rachel’s keys back to her. She lent them to me for the weekend. I always get up early. I need to put on my shoes. I need to put my shoes on. I take my shoes and put them on.",
+  },
+  {
+    tags: [
+      "Noun-существительное",
+      "Quantifiers-количественные местоимения",
+      "The Article-артикли",
+      "The, a, an",
+    ],
+    title: "Determiners-определители and Articles",
+    content:
+      "It's a computer. The computer isn’t modern, it's old. This computer is big, but these days we prefer fast and smart gadgets. It's a computer. My mother is a doctor. Give me an apple, please. Open the door, please. — What is this? — This is a box of matches.You can take these apples. Can you give me that pen, please? Those houses are new. I work for Microsoft. I work for an American company. I work for the European Union. I work for a newspaper. I work with many people. I work with a computer. I work in a hospital.I work in a shop. I work in an office. I work at home. I study at university/shool/work. I earn a lof money",
+  },
+  {
+    tags: [
+      "Noun-существительное",
+      "Quantifiers-количественные местоимения",
+      "The Article-артикли",
+    ],
+    title: "Quantifiers",
+    content:
+      "I don’t like the weather in London. There’s too much rain and not enough sun. It’s also not warm enough for me, and it’s too windy. Alex tried three hats at the store. The green hat was too big, the red one wasn’t big enough. The blue hat was big enough and cheap enough, so Alex bought it. I have too much work today. There are too many tasks and not enough time. This tea is too hot. I’ve waited long enough. The Christmas tree isn’t tall enough.  Our son has too many toys. Let’s sell some of them. You added too much salt to this soup. We can’t eat it. I have enough money to buy a new laptop. We baked enough cakes for all the guests at the party. There aren’t enough seats in the room for everyone. We didn’t have enough time to finish the test. The coffee is too hot. Don’t drink it too fast. Your handwriting is too messy because you’re writing too quickly. He plays well enough to win the championship. This house is cheap enough for Mary to buy. You woke the baby! You didn’t talk quietly enough. I can’t read this book. It’s not interesting enough.",
+  },
+  {
+    tags: ["Adverb-наречия"],
+    title: "Adverbs-наречия",
+    content:
+      "I walk quietly, talk politely and run fast. What about you? Listen carefully! I went to bed early yesterday and slept well. Our team played well last week. Use them to say how somebody does something or how something happens. Please, talk slowly. I can't understand you. I know the words of the song perfectly.",
+  },
+  {
+    tags: ["Adverb-наречия", "Comparative Degree-сравнительная степень"],
+    title:
+      "Comparative adjectives- сравнительные прилагательные and adverbs-наречия",
+    content: "",
+  },
+  {
+    tags: [
+      "Adjective-прилагательное",
+      "Adverb-наречия",
+      "Comparative Degree-сравнительная степень",
+    ],
+    title:
+      "Comparative adjectives- сравнительные прилагательные and adverbs-наречия",
+    content:
+      "Canada is bigger than China, although the population of China is larger. The Canadians are happier than other cultures.China's economy is more powerful. The Chinese language is more difficult than English. Chinese people drive worse than Canadians. Canada is bigger than China. This story is more interesting than the previous one. I'm taller than you. I drive more safely than my wife. We travel more often than our grandparents. Kate learns as fast as Tom.These glasses are as good as my old ones. The more you study, the better results you get. The faster you drive, the closer you’re to the destination.",
+  },
+  {
+    tags: [
+      "Adjective-прилагательное",
+      "Superlative Degree-превосходная степень",
+    ],
+    title: "Superlative adjectives-прилагательное and adverbs-наречия",
+    content:
+      "Let's play a quick question game! Who is the tallest person in your family? Let's play a quick question game! Who is the tallest person in your family? What is the most interesting film you've ever seen? The Green Mile. Where can I get the best coffee in your hometown? In the coffee house called 'Black'. What is the most beautiful place you've ever been to? The church in the mountains. My brother is the tallest person in the family. My phone is the most valuable thing in life. What is the most interesting film you've ever seen? It's the most beautiful picture I've ever seen.She is the kindest person I've ever known.",
+  },
+  {
+    tags: ["Adjective-прилагательное"],
+    title: "Common Adjectives-часто уп. прилагательные",
+    content:
+      "The TV set is old. Can you see these little kittens? The TV set is old. He has an expensive watch. Can you see these little kittens? My son is so funny. Her daughter is so funny.",
+  },
+  {
+    tags: ["Pronoun-местоимение", "Relative"],
+    title: "Defining Relative Clauses-придаточные предложения",
+    content:
+      "Cocoa is a powder which is used to make chocolate. But farmers who grow cocoa beans earn little money. Ivory Coast and Ghana are the countries where 70% of cocoa beans come from. A town hall is a place where a local government works. The item which/that I use every day is my laptop. The person who/that helps me a lot is my colleague.",
+  },
+  {
+    tags: ["Pronuoun-местоимение", "Demonstrative"],
+    title: "Demonstrative Pronouns-указательные местоимения",
+    content:
+      "This hotel is expensive, but it’s very nice. 'Who’s that girl?' 'I don’t know.' Do you like these shoes? I bought them last week. Those apples look nice. Can I have one? This is a nice hotel, but it’s very expensive. 'Excuse me, is this your bag?' 'Oh, yes, thank you.' Who’s that? (= Who is that person?). Which shoes do you prefer — these or those? 'I’m sorry I forgot to phone you.' 'That’s all right.' That was a really nice meal. Thank you very much.' 'You’re a teacher, aren’t you?' 'Yes, that’s right.' 'Martin has got a new job.' 'Has he? I didn’t know that.' 'I’m going on holiday next week.' 'Oh, that’s nice.'",
+  },
+  {
+    tags: ["Sentence Structure", "Word Order"],
+    title: "Word Order in Questions",
+    content:
+      "When does school begin in the UK? How did Gulliver get to Lilliput? Where is Fire Island? Do animals feel upset? Why are American colleges so expensive? When was the Great Fire of London?Where are the 2024 Summer Olympics going to take place? Where were you at 5 o'clock? Who was your best friend at school? How did you get home yesterday? Did she copy the test answers? Why are you often late? When is Thanksgiving day? How often does it rain there?Why do they think that it's a bad idea? Hey, where are you going?What is Rita wearing today? When are you going to buy a new fridge?Is he going to give away his clothes? Can you swim? What computer games can you play?",
+  },
+];
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {data.map((i, ind) => (
+        <div className="content" key={ind}>
+          <h2>{i.title}</h2>
+          <div className="tags">
+            {i.tags.map((t, ind) => (
+              <p className="tag" key={ind}>
+                {t}
+              </p>
+            ))}
+          </div>
+          <p>{i.content}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
